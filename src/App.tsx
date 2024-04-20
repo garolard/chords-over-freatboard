1b789d9e3c10e4b78cb4bea27500b9020a40f892
Gabriel Ferreiro <gbril9119@gmail.com>
2024-04-20 14:07:31 +0200

    Implements basic freatboard functionallity painting basic triad chords shapes

--- src/App.tsx
+++ src/App.tsx
@@ -10,6 +10,7 @@ import Freatboard from './components/Freatboard'
 function App() {
   const [queryParams, setQueryParams] = useQueryParams({ key: 'C', scale: 'major' });
   const availableChords = Chords.getChordsFor(queryParams.get('key')!, queryParams.get('scale')!);
+  const selectedChord = availableChords.find(chord => chord.name === queryParams.get('chord')) || availableChords[0];
 
   return (
     <div className='w-full flex flex-col'>
@@ -21,7 +22,7 @@ function App() {
         <KeySelector selectedKey={queryParams.get('key')!} setQueryParams={setQueryParams} />
         <ScaleSelector selectedScale={queryParams.get('scale')!} setQueryParams={setQueryParams} />
         <ChordSelector selectedChord={queryParams.get('chord') || availableChords[0].name} setQueryParams={setQueryParams} availableChords={availableChords} />
-        <Freatboard />
+        <Freatboard selectedChord={selectedChord}/>
       </div>
     </div>
   )
--- src/components/Freatboard.tsx
+++ src/components/Freatboard.tsx
@@ -1,26 +1,51 @@
+import React from 'react';
 import { getScaleFromRoot } from '../helpers/ChordsHelper';
+import { Chord } from '../types';
 
 const naturalIntervals = [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1];
 
-export default function Freatboard() {
-	return <div className="w-full grid grid-cols-12 grid-rows-6 mt-40">
+type FreatboardProps = {
+	selectedChord: Chord;
+};
+export default function Freatboard({ selectedChord }: FreatboardProps) {
+	return <div className="w-full grid grid-cols-12 grid-rows-6 gap-2 mt-40">
 		{getScaleFromRoot('E', naturalIntervals).map((note, i) => (
-			<div key={i} className="border border-gray-medium">{note}</div>
+			<FreatboardNote key={i} note={note} chordNotes={selectedChord.notes} />
 		))}
-	{getScaleFromRoot('B', naturalIntervals).map((note, i) => (
-			<div key={i} className="border border-gray-medium">{note}</div>
+		{getScaleFromRoot('B', naturalIntervals).map((note, i) => (
+			<FreatboardNote key={i} note={note} chordNotes={selectedChord.notes} />
 		))}	
 		{getScaleFromRoot('G', naturalIntervals).map((note, i) => (
-			<div key={i} className="border border-gray-medium">{note}</div>
+			<FreatboardNote key={i} note={note} chordNotes={selectedChord.notes} />
 		))}
 		{getScaleFromRoot('D', naturalIntervals).map((note, i) => (
-			<div key={i} className="border border-gray-medium">{note}</div>
+			<FreatboardNote key={i} note={note} chordNotes={selectedChord.notes} />
 		))}
 		{getScaleFromRoot('A', naturalIntervals).map((note, i) => (
-			<div key={i} className="border border-gray-medium">{note}</div>
+			<FreatboardNote key={i} note={note} chordNotes={selectedChord.notes} />
 		))}
 		{getScaleFromRoot('E', naturalIntervals).map((note, i) => (
-			<div key={i} className="border border-gray-medium">{note}</div>
+			<FreatboardNote key={i} note={note} chordNotes={selectedChord.notes} />
 		))}
+	</div>;
+}
+
+type FreatboardNoteProps = {
+	note: string;
+	chordNotes: string[];
+};
+function FreatboardNote({ note, chordNotes, ...props }: FreatboardNoteProps) {
+	const noteGradeOnChord = chordNotes.indexOf(note);
+	let bgColor = '';
+	if (noteGradeOnChord === 0) {
+		bgColor = 'bg-green-400';
+	} else if (noteGradeOnChord === 1 || noteGradeOnChord === 2) {
+		bgColor = 'bg-yellow-400';
+	}
+
+	return <div className="flex justify-center">
+		<div className={`p-2 px-4 border border-gray-200 rounded-full ${bgColor}`}>
+			{note}
+		</div>
 	</div>;
 }
